04f90e4b9e257bb13de7fc6c09555655a57e0fc8
Дмитрий Гавриш <demetriusgarson@gmail.com>
2026-06-11 00:21:25 +0300

    HW2 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,36 +1,19 @@
-// Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
+const categories = {
+  categoriesNames: [...document.querySelectorAll('.item')],
 
-// Використай цей стартовий код і виконай рефакторинг. Після оголошення об'єкта ми додали виклики методів. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+  showNumberOfCategories() {
+    console.log(`Number of Categories: ${this.categoriesNames.length}`);
+  },
 
-console.log('HW6_task-1:');
-
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
+  showCategoriesNames() {
+    for (const category of this.categoriesNames) {
+      console.log(`Category: ${category.querySelector('h2').textContent}`);
+      console.log(
+        `Elements: ${category.querySelector('ul').querySelectorAll('li').length}`
+      );
+    }
   },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
 };
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+categories.showNumberOfCategories();
+categories.showCategoriesNames();
